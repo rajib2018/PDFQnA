effebfc89af376ab7d7d3271b40d8299cc79b543
Rajib Bhattacharya <38496316+rajib2018@users.noreply.github.com>
2025-09-07 14:29:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import traceback
 # You can obtain a token from your Hugging Face account settings
 # It's recommended to use Streamlit secrets for actual deployment
 # Ensure this token is set before the app logic
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = "Nhf_hNmlmYDithuvpSyxGbiGzyADmJOwMPgogs
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "Nhf_hNmlmYDithuvpSyxGbiGzyADmJOwMPgogs"
 
 def process_pdf(uploaded_file):
     """Processes the uploaded PDF, creates a vector store."""
